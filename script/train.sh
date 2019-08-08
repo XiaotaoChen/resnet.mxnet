99ed567cbc2169bdb8cb42d95ed0012366cb2ed1
xiaotao.chen <xiaotao.chen@tusimple.ai>
2019-08-08 12:23:42 +0000

    suppport truenas

--- script/train.sh
+++ script/train.sh
@@ -2,6 +2,8 @@
 
 export PYTHONPATH="/mnt/tscpfs/xiaotao.chen/Repositories/mxnet_zjq/python:${PYTHONPATH}"
 workspace="/mnt/tscpfs/xiaotao.chen/Repositories/resnet.mxnet"
+#export PYTHONPATH="/mnt/truenas/scratch/xiaotao.chen/Repositories/incubator-mxnet/python:${PYTHONPATH}"
+#workspace="/mnt/truenas/scratch/xiaotao.chen/Repositories/resnet.mxnet"
 
 
 cd ${workspace}
