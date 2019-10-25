056d2040bc16c9661eab63633b5e4887bd0d4442
xiaotao.chen <xiaotao.chen@tusimple.ai>
2019-10-25 03:04:01 +0000

    add resent_imagenet match with distiller model

--- symbol/__init__.py
+++ symbol/__init__.py
@@ -1,5 +1,6 @@
 from .resnet import resnet
 from .resnet import resnet_cifar10
+from .resnet import resnet_imagenet
 from .resnext import resnext
 from .resnext_cyt import resnext as resnext_cyt
 from .resnet_mxnet import resnet_mxnet
--- symbol/resnet.py
+++ symbol/resnet.py
@@ -146,3 +146,45 @@ def resnet_cifar10(units, num_stage, filter_list, num_classes, data_type, bottle
     fc1 = mx.symbol.FullyConnected(data=flat, num_hidden=num_classes, name='fc1')
     cls = mx.symbol.SoftmaxOutput(data=fc1, name='softmax')
     return cls
+
+def resnet_imagenet(units, num_stage, filter_list, num_classes, data_type, bottle_neck=True,
+           bn_mom=0.9, workspace=512, memonger=False, grad_scale=1.0, dataset_type=None):
+    print("units:{}, num_stage:{}, filter_list:{}, bottle_neck:{}".format(units, num_stage, filter_list, bottle_neck))
+
+    num_unit = len(units)
+    assert (num_unit == num_stage)
+
+    data = mx.sym.Variable(name='data')
+
+    data = mx.sym.BatchNorm(data=data, fix_gamma=True, eps=2e-5, momentum=bn_mom, name='bn_data')
+
+    body = mx.sym.Convolution(data=data, num_filter=filter_list[0], kernel=(7, 7), stride=(2, 2), pad=(3, 3),
+                                no_bias=True, name="conv0", workspace=workspace)
+    body = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=eps, momentum=bn_mom, name='bn0')
+    body = mx.sym.Activation(data=body, act_type='relu', name='relu0')
+    body = mx.symbol.Pooling(data=body, kernel=(3, 3), stride=(2, 2), pad=(1, 1), pool_type='max')
+
+    for i in range(num_stage):
+        if i == 0:
+            name='stage1_unit1'
+            conv1 = mx.sym.Convolution(data=body, num_filter=filter_list[i + 1], kernel=(3, 3), stride=(1,1), pad=(1, 1),
+                                        no_bias=True, workspace=workspace, name=name + '_conv1')
+            bn2 = mx.sym.BatchNorm(data=conv1, fix_gamma=False, momentum=bn_mom, eps=eps, name=name + '_bn2')
+            act2 = mx.sym.Activation(data=bn2, act_type='relu', name=name + '_relu2')
+            conv2 = mx.sym.Convolution(data=act2, num_filter=filter_list[i + 1], kernel=(3, 3), stride=(1, 1), pad=(1, 1),
+                                        no_bias=True, workspace=workspace, name=name + '_conv2')
+            body = conv2 + body
+        else:
+            body = residual_unit(body, filter_list[i + 1], (1 if i == 0 else 2, 1 if i == 0 else 2), False,
+                                name='stage%d_unit%d' % (i + 1, 1), bottle_neck=bottle_neck, workspace=workspace,
+                                memonger=memonger)
+        for j in range(units[i] - 1):
+            body = residual_unit(body, filter_list[i + 1], (1, 1), True, name='stage%d_unit%d' % (i + 1, j + 2),
+                                 bottle_neck=bottle_neck, workspace=workspace, memonger=memonger)
+    bn1 = mx.sym.BatchNorm(data=body, fix_gamma=False, eps=eps, momentum=bn_mom, name='bn1')
+    relu1 = mx.sym.Activation(data=bn1, act_type='relu', name='relu1')
+    pool1 = mx.symbol.Pooling(data=relu1, global_pool=True, kernel=(7, 7), pool_type='avg', name='pool1')
+    flat = mx.symbol.Flatten(data=pool1)
+    fc1 = mx.symbol.FullyConnected(data=flat, num_hidden=num_classes, name='fc1')
+    cls = mx.symbol.SoftmaxOutput(data=fc1, name='softmax')
+    return cls
